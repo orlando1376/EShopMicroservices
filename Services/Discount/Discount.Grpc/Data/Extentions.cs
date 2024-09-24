b552196ba927852e27c471ec83956b8c9566db18
DIGITALWARE\OrlandoF <orlandof@digitalware.com.co>
2024-09-24 18:14:32 -0500

    Crear servicio Discount.Grpc

---
+++ Services/Discount/Discount.Grpc/Data/Extentions.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Discount.Grpc.Data
+{
+    /// <summary>
+    /// Método de extensión para hacer automigraciones
+    /// </summary>
+    public static class Extentions
+    {
+        public static IApplicationBuilder UseMigration(this IApplicationBuilder app)
+        {
+            using var scope = app.ApplicationServices.CreateScope();
+            using var dbContext = scope.ServiceProvider.GetRequiredService<DiscountContext>();
+            dbContext.Database.MigrateAsync();
+
+            return app;
+        }
+    }
+}
